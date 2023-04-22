23ca685a433fd837061d10f51ce3db3de5075c74
gmhartman <108906460+gmhartman@users.noreply.github.com>
2023-04-22 18:32:37 -0500

    save 8

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const { Configuration, OpenAIApi } = require("openai");
 require('dotenv').config();
-const path = require('path');
+// const path = require('path');
 const express = require("express");
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const chatAPIKey = process.env.CHATGPT_API_KEY;
-const sslRedirect = require('heroku-ssl-redirect');
+// const sslRedirect = require('heroku-ssl-redirect');
 
 const configuration = new Configuration({
     organization: "org-OopekGf8qFEOTNI3a5ES7fI5",
@@ -18,18 +18,18 @@ app.use(bodyParser.json())
 app.use(cors())
 
 //remove if not working
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
-app.use(sslRedirect());
+// app.use(sslRedirect());
 
 app.post('/', async (req, res) => {
     const { message } = req.body;
